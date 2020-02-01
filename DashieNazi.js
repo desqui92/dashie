cf08b0adede8632779010e0b30f6aa09a96328ff
desqui92 <43731462+desqui92@users.noreply.github.com>
2020-02-01 07:47:02 -0300

    hkjhjkh

--- DashieNazi.js
+++ DashieNazi.js
@@ -343,7 +343,7 @@ client.on('guildMemberAdd', async member => {
     //ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
 
     // Add an exclamation point here and below
-    ctx.font = applyText(28, `${member.displayName}`);
+    ctx.font = '28px';
     ctx.fillStyle = '#ff0000';
     ctx.fillText(`${member.displayName}`, canvas.width/4.5, canvas.height / 1.8);
 
