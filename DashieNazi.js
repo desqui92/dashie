ae59ba1419d75724a64576a97489b28ba7efcb09
desqui92 <43731462+desqui92@users.noreply.github.com>
2020-02-01 06:06:35 -0300

    fhgf

--- DashieNazi.js
+++ DashieNazi.js
@@ -349,7 +349,7 @@ client.on('guildMemberAdd', async member => {
     ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
 
     ctx.beginPath();
-    ctx.arc(310, 140, 65, 0, 2 * Math.PI, true);
+    ctx.arc(320, 140, 65, 0, 2 * Math.PI, true);
     //ctx.rect(20,20,200,200);
     //ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
     ctx.closePath();
