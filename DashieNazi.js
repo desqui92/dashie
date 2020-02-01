710f215389301352c8d924c02b594eb78f3be58b
desqui92 <43731462+desqui92@users.noreply.github.com>
2020-02-01 06:43:05 -0300

    gjhg

--- DashieNazi.js
+++ DashieNazi.js
@@ -357,7 +357,7 @@ client.on('guildMemberAdd', async member => {
 
     const { body: buffer } = await snekfetch.get(member.user.displayAvatarURL);
     const avatar = await Canvas.loadImage(buffer);
-    ctx.drawImage(avatar, 260, 80, 120, 120);
+    ctx.drawImage(avatar, 260, 80, 125, 125);
 
     const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
 
