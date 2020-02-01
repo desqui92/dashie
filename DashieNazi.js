2b0648abfe22fd720537b177bfed8e7c4989b4a8
desqui92 <43731462+desqui92@users.noreply.github.com>
2020-02-01 07:12:26 -0300

    vccnc

--- DashieNazi.js
+++ DashieNazi.js
@@ -345,9 +345,9 @@ client.on('guildMemberAdd', async member => {
 
     // Add an exclamation point here and below
     //ctx.font = applyText(canvas, `${member.displayName}!`);
-    ctx.font = '24px manicdepress red';
+    ctx.font = '26px red';
     ctx.fillStyle = '#ffffff';
-    var vari = (canvas.width/5.5) - ((ctx.measureText(`${member.displayName}`).width)/2);
+    var vari = (canvas.width/5) - ((ctx.measureText(`${member.displayName}`).width)/2);
     ctx.fillText(`${member.displayName}`, vari, canvas.height / 1.8);
 
     ctx.beginPath();
