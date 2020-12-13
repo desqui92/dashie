c9e605d4d4bf5591df1e007397002bc274541a51
desqui92 <43731462+desqui92@users.noreply.github.com>
2020-12-13 17:52:12 -0300

    m
    m

--- DashieNazi.js
+++ DashieNazi.js
@@ -23,13 +23,4 @@ var emitter = new MyEmitter();
 client.login(process.env.BOT_TOKEN);
 //lel
 
-bot.on('message', msg => { // Message function
-   if (msg.author.bot) return; // Ignore all bots
-   if (msg.content.startsWith(settings.prefix)) return; // It always has to starts with the prefix which is '!'
-
-   if (msg.content.startsWith(settings.prefix + "pingg")) { // When a player does '!ping'
-     msg.reply("Pong!") // The bot will say @Author, Pong!
-   }
-});
-
 
